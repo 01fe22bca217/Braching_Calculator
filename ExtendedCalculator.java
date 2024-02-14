7ec5ae4116f7df8cddeebed4c9bc3413a363a73c
Rakshita <01fe22bca217@kletech.ac.in>
2024-02-14 15:23:16 +0530

    adding two files to branch

---
+++ ExtendedCalculator.java
@@ -0,0 +1,12 @@
+public class Extendedcalculator{
+    float modulus(float operand_1,operand_2){
+        return(operand_1%operand_2);
+
+    }
+    float areaoftriangle(float base,float height){
+        return(0.5*base*height);
+    }
+
+    
+    
+}
